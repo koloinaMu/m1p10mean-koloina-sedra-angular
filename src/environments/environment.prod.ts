38bbdcaf42049cf37f51a8cd4d85aa5de5241816
Koloina <rabenjamu@gmail.com>
2023-02-24 00:26:14 +0400

    changement url node

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //baseUrl:'http://localhost:3000/node/'
-  baseUrl:'https://m1p10mean-koloina-sedra-deploy-production.up.railway.app/node/'
+  //baseUrl:'https://m1p10mean-koloina-sedra-deploy-production.up.railway.app/node/'
+  baseUrl:'https://m1p10mean-koloina-sedra-node-2.onrender.com/'
 };
